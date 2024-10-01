ab65bd5e6cc4168acb5e81b6917615c1cd01993e
dlucioyauh <dlucio.douglas@gmail.com>
2024-10-01 17:48:46 -0300

    Modificando Estruturas e designe

--- target/classes/static/css/js/script.js
+++ target/classes/static/css/js/script.js
@@ -11,6 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('visible');
                 observer.unobserve(entry.target); // Para não animar novamente
+            } else {
+                entry.target.classList.remove('visible');
             }
         });
     }, options);
@@ -24,6 +26,44 @@ document.addEventListener('DOMContentLoaded', () => {
         item.addEventListener('click', () => {
             menuItems.forEach(link => link.classList.remove('active'));
             item.classList.add('active');
+        });
+    });
+
+    // Funcionalidade do botão do menu
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav ul');
+
+    menuToggle.addEventListener('click', () => {
+        nav.classList.toggle('show'); // Alterna a classe 'show' para mostrar/esconder o menu
+    });
+
+    // Rolagem suave
+    menuItems.forEach(item => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = item.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Exibir botão "Voltar ao topo"
+    const goTopButton = document.getElementById("go-top");
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 200) {
+            goTopButton.style.display = "block";
+        } else {
+            goTopButton.style.display = "none";
+        }
+    });
+
+    goTopButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
         });
     });
 });
